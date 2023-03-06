2a1c5fd6eda5fe2382d1856297fb01db1e73d130
pirmanduiana <pirmandwiana@gmail.com>
2023-03-06 12:43:40 +0800

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
         component: FinderComponent
     },
     {
+        path: 'registered',
+        component: FinderComponent
+    },
+    {
         path: 'terms',
         component: TermsComponent
     }
