ae37fa24d2f626539afd8734e1b2e6b3545d4945
pirmanduiana <pirmandwiana@gmail.com>
2023-03-06 21:02:43 +0800

    -

--- src/app/pxdetail/pxdetail.component.ts
+++ src/app/pxdetail/pxdetail.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pxdetail',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./pxdetail.component.css']
 })
 export class PxdetailComponent {
+  
+  id: number = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location
+  ){}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+  }
+
+  back(): void
+  {
+    this.location.back();
+  }
 
 }
