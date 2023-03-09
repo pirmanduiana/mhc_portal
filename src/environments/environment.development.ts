4ee9b8b789f099ab1f730deded34d4f837dff09b
pirmanduiana <pirmandwiana@gmail.com>
2023-03-09 11:03:10 +0800

    registerd

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "https://mhc-club.com/api/"
+    apiUrl: "http://localhost:1234/api/"
+    // apiUrl: "https://mhc-club.com/api/"
 };
